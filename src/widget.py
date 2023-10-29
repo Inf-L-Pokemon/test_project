897d56e86c3e643bac8d20866087781944bf0795
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-10-29 20:23:22 +0300

    add docstring to def type_and_hidden_number on widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,12 @@ from masks import hidden_card_number, hidden_account_number
 
 
 def type_and_hidden_number(type_number: str) -> str:
+    """
+    Принимает на вход строку с информацией тип карты/счета и номер карты/счета,
+    возвращает эту строку с замаскированным номером карты/счета
+    :param type_number: Строка с информацией "тип карты/счета и номер карты/счета"
+    :return: Тип карты/счета с замаскированным номером карты/счета
+    """
     index_start_number = type_number.index(str(range(10)))
     if len(type_number[index_start_number:]) == 16:
         return type_number[:index_start_number] + hidden_card_number(type_number[index_start_number:])
