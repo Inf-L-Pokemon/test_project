4b73fbea32920b722881f40c9956aaeaacbf9582
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-11-04 18:52:02 +0300

    del test_masks.py (functions from "masks.py" are tested in the "test_widget.py" module) add tests in test_processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,63 @@
+import pytest
+
+from src.processing import date_sorting_transactions, state_sorting_transactions
+
+
+@pytest.fixture
+def list_dict():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.mark.parametrize(
+    "state, expected_result",
+    [
+        (
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_state_sorting_transactions(list_dict, state, expected_result):
+    assert state_sorting_transactions(list_dict, state=state) == expected_result
+
+
+@pytest.mark.parametrize(
+    "reverse_key, expected_result",
+    [
+        (
+            True,
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            False,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
+    ],
+)
+def test_date_sorting_transactions(list_dict, reverse_key, expected_result):
+    assert date_sorting_transactions(list_dict, reverse_key=reverse_key) == expected_result
