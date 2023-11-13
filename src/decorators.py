3e4a2766c141bdcbc2d288ce837f8ff2363d1fb1
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-11-13 22:50:35 +0300

    refactor code in decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -7,22 +7,19 @@ def log(filename: str | None = None) -> Optional[Callable]:
     def wrapper(func: Callable) -> Optional[Callable]:
         @wraps(func)
         def inner(*args: Any, **kwargs: Any) -> Any:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             try:
                 result = func(*args, **kwargs)
-                now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-                if filename:
-                    with open(filename, "a", encoding="utf8") as log_file:
-                        log_file.write(f"{now} {func.__name__} ok\n")
-                else:
-                    print(f"{now} {func.__name__} ok")
-                return result
+                log_message = f"{now} {func.__name__} ok\n"
             except Exception as err:
-                now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-                if filename:
-                    with open(filename, "a", encoding="utf8") as log_file:
-                        log_file.write(f"{now} {func.__name__} error: {err}. Inputs: {args}, {kwargs}\n")
-                else:
-                    print(f"{now} {func.__name__} error: {err}. Inputs: {args}, {kwargs}")
+                log_message = f"{now} {func.__name__} error: {err}. Inputs: {args}, {kwargs}\n"
+                result = None
+            if filename:
+                with open(filename, "a", encoding="utf8") as log_file:
+                    log_file.write(log_message)
+            else:
+                print(log_message)
+            return result
 
         return inner
 
