8ba02c8f9f43648647598dec3739463fc357fa4d
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-11-19 20:11:45 +0300

    add test_utils.py and some change utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import json
+from typing import Any
 
 
-def return_transaction_from_json(filepath: str) -> list:
+def return_transaction_from_json(filepath: str) -> list | Any:
     """
     Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
@@ -9,12 +10,10 @@ def return_transaction_from_json(filepath: str) -> list:
     :return: Список транзакций (опционально - пустой список).
     """
     try:
-        with open(filepath, "r", encoding='utf-8') as jf:
+        with open(filepath, "r", encoding="utf-8") as jf:
             list_transaction = json.load(jf)
-            if not isinstance(list_transaction, list):
-                return []
             return list_transaction
-    except FileNotFoundError:
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
         return []
 
 
@@ -22,7 +21,7 @@ def transaction_in_rubles(transaction: dict) -> float | ValueError:
     """
     Принимает на вход одну транзакцию и возвращает сумму транзакции в рублях.
     :param transaction: Транзакция в виде словаря.
-    :return: Сумма транзакции в рублях (ошибку ValueError с сообщением
+    :return: Сумма транзакции в рублях (выдаёт ошибку ValueError с сообщением
     "Транзакция выполнена не в рублях. Укажите транзакцию в рублях", если транзакция была совершена в другой валюте).
     """
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
