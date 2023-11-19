721e6885afd8f42f077909fb04be5ad8e0d523a4
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-11-19 18:13:31 +0300

    add operations.json and utils.py

---
+++ src/utils.py
@@ -0,0 +1,31 @@
+import json
+
+
+def return_transaction_from_json(filepath: str) -> list:
+    """
+    Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+    :param filepath: Путь до JSON-файла.
+    :return: Список транзакций (опционально - пустой список).
+    """
+    try:
+        with open(filepath, "r", encoding='utf-8') as jf:
+            list_transaction = json.load(jf)
+            if not isinstance(list_transaction, list):
+                return []
+            return list_transaction
+    except FileNotFoundError:
+        return []
+
+
+def transaction_in_rubles(transaction: dict) -> float | ValueError:
+    """
+    Принимает на вход одну транзакцию и возвращает сумму транзакции в рублях.
+    :param transaction: Транзакция в виде словаря.
+    :return: Сумма транзакции в рублях (ошибку ValueError с сообщением
+    "Транзакция выполнена не в рублях. Укажите транзакцию в рублях", если транзакция была совершена в другой валюте).
+    """
+    if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        return float(transaction["operationAmount"]["amount"])
+    else:
+        raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
